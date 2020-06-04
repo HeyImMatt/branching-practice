f6464ffb0317684074230eb42ad5733e0b8a277b
Elizabeth Van Hook <evanhook360@gmail.com>
2020-06-04 11:02:44 -0700

    add show and hide to each animal in js
    Co-authored-by: Matt McFarland <mrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   $(".container").find("#giraffeInfo, #koalaInfo, #whaleInfo").click(function() {
-    $(this).find('p').toggleClass("hidden");
-    console.dir(this);
+    $(this).find("p").toggleClass("hidden");
+    if (this.id === "giraffeInfo") {
+      $("#koalaInfo, #whaleInfo").find("p").addClass("hidden");
+    } else if (this.id === "koalaInfo") {
+      $("#giraffeInfo, #whaleInfo").find("p").addClass("hidden");
+    } else {
+      $("#giraffeInfo, #koalaInfo").find("p").addClass("hidden");
+    }
   })
 })
